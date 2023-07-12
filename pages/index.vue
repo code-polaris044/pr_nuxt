f064807a8fbd0e651bdd4ffafbd04fbc3e8925ae
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-13 00:30:45 +0900

    画像追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
   <h1 class="title">Main Page</h1>
   <p>テストサイト</p>
+  <img src="~/assets/img/a.jpg" alt="test" class="img" />
 </template>
 <style lang="scss" scoped>
 .title {
   background: red;
+}
+
+.img {
+  width: 100%;
+  height: auto;
+  aspect-ratio: 2/1;
+  object-fit: cover;
 }
 </style>
