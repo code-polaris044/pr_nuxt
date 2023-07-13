7893f6d5e10035dfec6d9ebefd5bd7f23795cf7e
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-14 00:00:01 +0900

    画像変更

--- pages/curry.vue
+++ pages/curry.vue
@@ -15,6 +15,60 @@
       <div class="curry__imgWrap">
         <img src="~/assets/img/curry3.jpg" alt="curry" class="curry__img" />
       </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry4.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry5.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry6.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry7.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry8.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry9.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry10.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry11.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry12.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry13.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry14.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry15.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry16.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry17.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry18.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry19.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry20.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry21.jpg" alt="curry" class="curry__img" />
+      </div>
     </div>
   </section>
 </template>
@@ -33,11 +87,28 @@
   width: $common-contents-width-pc;
   margin: 0 auto 100px;
   display: flex;
-  gap: 40px;
+  column-gap: 40px;
+  row-gap: 60px;
   flex-wrap: wrap;
 
   @include tab {
     width: $common-contents-width-tab;
+    column-gap: 30px;
+  }
+}
+
+.curry__imgWrap {
+  width: calc((100% - (40px * 2)) / 3);
+
+  @include tab {
+    width: calc((100% - (30px * 1)) / 2);
+  }
+
+  .curry__img {
+    width: 100%;
+    height: auto;
+    aspect-ratio: 16/9;
+    object-fit: cover;
   }
 }
 </style>
