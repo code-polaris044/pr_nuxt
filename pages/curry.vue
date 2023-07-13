bdd2d472f79af0716ad802e3d6fd1316fdcf8e66
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-13 23:45:47 +0900

    カレー画像追加

--- pages/curry.vue
+++ pages/curry.vue
@@ -4,6 +4,19 @@
       <h2 class="curry__title">Favorite Curry</h2>
     </div>
   </main>
+  <section class="curry">
+    <div class="curry__wrap">
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry1.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry2.jpg" alt="curry" class="curry__img" />
+      </div>
+      <div class="curry__imgWrap">
+        <img src="~/assets/img/curry3.jpg" alt="curry" class="curry__img" />
+      </div>
+    </div>
+  </section>
 </template>
 <style lang="scss" scoped>
 .curry__title {
@@ -14,5 +27,17 @@
   letter-spacing: 0.1em;
   font-weight: 600;
   color: $c-assort;
+}
+
+.curry__wrap {
+  width: $common-contents-width-pc;
+  margin: 0 auto 100px;
+  display: flex;
+  gap: 40px;
+  flex-wrap: wrap;
+
+  @include tab {
+    width: $common-contents-width-tab;
+  }
 }
 </style>
