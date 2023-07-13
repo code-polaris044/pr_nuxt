d06b8dd644aa1036245bc121820630bc0612b0b6
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-13 13:27:20 +0900

    header add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  build: {
+    transpile: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/vue-fontawesome",
+    ],
+  },
+  css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+  plugins: ["@/plugins/fontawesome.ts"],
   app: {
     head: {
       charset: "utf-8",
